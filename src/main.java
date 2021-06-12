bd1cc9fb2c114c2bc37afcd025542006a06b202c
GustiGilang <gustigilang98@gmail.com>
2021-06-12 13:13:48 +0800

    Membuat Main Class

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main (String[]args){
+
+        String namaDepan = "Gusti";
+        String namaTengah = "Gilang";
+        String namaBelakang = "Haifa Fadillah";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println (namaDepan + namaTengah + namaBelakang);
+    }
+}
